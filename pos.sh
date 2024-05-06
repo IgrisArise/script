59312968127ed1174cc52d52358d91fa4f1796fe
Igris <155902159+Igris-Arise@users.noreply.github.com>
2024-05-07 00:56:59 +0530

    adapt matrix

--- pos.sh
+++ pos.sh
@@ -1,12 +1,12 @@
 rm -rf .repo/local_manifests/
 #repo init rom
-repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs
+repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
 #Local manifests
-git clone https://github.com/IgrisArise/local_manifests -b pos .repo/local_manifests
+git clone https://github.com/IgrisArise/local_manifests -b main .repo/local_manifests
 
 #build
 /opt/crave/resync.sh
 . build/envsetup.sh
-lunch aosp_veux-ap1a-userdebug || lunch aosp_veux-userdebug
+lunch lineage_veux-ap1a-userdebug || lunch lineage_veux-userdebug
 make installclean
-make bacon
+brunch veux
