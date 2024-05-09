f295582091e5a54944881aa7951ab706206dc7ff
Igris <monuohlyan@duck.com>
2024-05-10 01:37:38 +0530

    Update pos.sh

--- pos.sh
+++ pos.sh
@@ -1,8 +1,17 @@
+#remove old local manifest
 rm -rf .repo/local_manifests/
+
 #repo init rom
 repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs
+
 #Local manifests
 git clone https://github.com/IgrisArise/local_manifests -b pos .repo/local_manifests
+
+#ccache
+sudo apt-get install ccache
+export USE_CCACHE=1
+export CCACHE_DIR=~/.ccache
+ccache -M 50G
 
 #build
 /opt/crave/resync.sh
